4ac521b08e460260acd4a4b8af1b10023f10dc6d
Глейм Иван <glejm@www.mts.ru>
2024-08-11 18:37:59 +0300

    removed key

--- src/api/generateSHA.ts
+++ src/api/generateSHA.ts
@@ -2,8 +2,6 @@
 // import { ECPairFactory, ECPairAPI } from 'ecpair'
 // import * as tinysecp from 'tiny-secp256k1'
 
-// const privateKeyWIF = 'L5CrWaQMhMep7RzVgR4gZE1DmJj6eyHirEBV2DxWqVyqTfSnV7Vp'
-
 // const ECPair: ECPairAPI = ECPairFactory(tinysecp)
 
 // export const signMessage = (message: Buffer) => {
